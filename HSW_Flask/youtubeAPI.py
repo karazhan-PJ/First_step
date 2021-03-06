bd849c94226e668a78a9fcfe82080f347a5c2a04
DESKTOP-RBBU304\허석원 <swheo92@gmail.com>
2021-03-06 22:52:08 +0900

    [2021-03-06] 유튜브 API 통신 테스트

--- HSW_Flask/youtubeAPI.py
+++ HSW_Flask/youtubeAPI.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from youtube_api import YouTubeDataAPI
 
-api_key = 'AIzaSyA0xZ5whsLAcleAFmDSwjwSUBaqX607GMs'
+api_key = ''
 
 yt=YouTubeDataAPI(api_key)
 
-tech_reviewer_search = yt.search(q='tech review', 
+tech_reviewer_search = yt.search(q='보겸TV', 
                           max_results = 5, 
                           order_by = 'viewCount', 
                           safe_search=None, 
