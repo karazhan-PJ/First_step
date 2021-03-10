65f7395499574275d744fe08d3043d6e29009a4d
DESKTOP-RBBU304\허석원 <swheo92@gmail.com>
2021-03-10 23:35:34 +0900

    [2021-03-10] Flask 모듈 구성 중

--- HSW_Flask/hello_world.py
+++ HSW_Flask/hello_world.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from flask import request
+from flask import make_response
+
+
 app = Flask(__name__)
+api = Api(app)
+
+class HttpSimilarIntent(Resource):
+    def get(self):
+	    return "hello"
 
 
 @app.route('/')
@@ -7,6 +17,7 @@ def hello_world():
 
 	return 'Hello World!'
 
+api.add_resource(HttpSimilarIntent, '/youtube/channelList')
 	
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000, debug=True)
