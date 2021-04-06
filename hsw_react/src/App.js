a3155da1337ebbc31099117b899d66340d6da5bb
DESKTOP-RBBU304\허석원 <swheo92@gmail.com>
2021-04-07 01:07:27 +0900

    [2021-04-06] React 구성

--- hsw_react/src/App.js
+++ hsw_react/src/App.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      Hello World
     </div>
   );
-}
-
-function Axios_test() {
-  axios({
-    url: 'http://test/api/cafe/list/today',
-    method: 'get',
-    data: {
-      foo: 'diary'
-    }
-  });
 }
 
 export default App;
--- hsw_react/src/index.js
+++ hsw_react/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './App';
 
 ReactDOM.render(
-   <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
