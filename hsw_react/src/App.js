fc8d756e81c46a4c2816abaeed129ba2c7554e88
DESKTOP-RBBU304\허석원 <swheo92@gmail.com>
2021-04-10 22:45:43 +0900

    [2021-04-10] Flask React  환경 구성

--- hsw_react/src/App.js
+++ hsw_react/src/App.js
@@ -1,5 +1,17 @@
+import axios from 'axios';
+
 
 function App() {
+  console.log("진입");
+  const url = "http://127.0.0.1:5000/youtube/channelList";
+  axios.get(url, { params: { product: "aaa" } })
+    .then(function(response) {
+        console.log("성공");
+    })
+    .catch(function(error) {
+        console.log("실패");
+    })
+  
   return (
     <div className="App">
       Hello World
